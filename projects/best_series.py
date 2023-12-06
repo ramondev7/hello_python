bb61ea3318ee3192c7fed606a25515003cae7522
ramondev7 <ramongch7@gmail.com>
2023-12-06 23:20:35 +0100

    Tupla con las mejores series del mundo mundial

--- projects/best_series.py
+++ projects/best_series.py
@@ -2,8 +2,11 @@
 # Tupla con las mejores series del mundo mundial, y como las tuplas son inmutables, son las 5 mejores series 100% real
 
 best_series = tuple()
-
 best_series = ('Sons of Anarchy', 'Game of Thrones', 'Breaking Bad', 'Peaky Blinders', 'Vikings')
+
+best_series_fake = tuple()
+best_series_fake = ('Amor eterno', 'Mi otra yo', 'Hermanos', 'Pasión de Gavilanes', 'Amor prohibido')
+del best_series_fake
 
 
 for i in best_series:
