0ee0c7103e7171f3d1cdd2abb3b42194e959f81b
ramondev7 <ramongch7@gmail.com>
2023-12-06 22:14:32 +0100

    Lista ordenada con las 'mejores' películas

--- projects/best_films.py
+++ projects/best_films.py
@@ -1,5 +1,5 @@
 # Escribiendo código con ❤️
-# Lista con las 'mejores' películas
+# Lista ordenada con las 'mejores' películas
 
 best_films = list()
 best_films = ['Oppenheimer', 'Agente Stone', 'Gran Turismo', 'Blue Beetle', 'Barbie']
@@ -9,5 +9,9 @@ best_films.insert(0, 'The Godfather')
 
 best_films.remove('Barbie')
 
-for film in range(len(best_films)):
-    print (best_films[film])
+best_films.sort()
+
+    
+for i in best_films:
+    print(i)
+    
