8a1ddc1912bcd3de6777241001270d023aef9665
ramondev7 <ramongch7@gmail.com>
2023-12-07 14:38:28 +0100

    Listado gamer desordenado (set)

---
+++ projects/games_list.py
@@ -0,0 +1,7 @@
+# Escribiendo código con ❤️
+# Set con un listado desordenado de videojuegos (por lo que no se puede acceder a elementos concretos), y tampoco se repiten los elementos
+
+list = set()
+list = {'GTA', 'Counter-Strike', 'Uncharted', 'Silent Hill', 'Chess'}
+
+print(f"El mejor juego de todos los tiempos es {list}")
